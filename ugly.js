dd46177c434b3f594ece53ba4a83e3e57a55bd21
Phil Barresi <me@philbarresi.com>
2018-02-23 14:16:58 -0500

    Added hoisting example

--- ugly.js
+++ ugly.js
@@ -1,5 +1,11 @@
+function hoist() {
+  return "AHOY! HOIST THE FLAGS!";
+}
+
 const test = ["foo", "bar", "bizz", "buzz"];
 
 console.log("this", "is", "a", "test", test);
 
 console.log("This is another line");
+
+console.log(hoist());
